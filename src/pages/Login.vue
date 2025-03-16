723af2cf80d80a12c75e01831515e97af27cf06d
Ashwin Sundar <ashwinjrdemon@gmail.com>
2025-03-16 18:43:11 +0800

    feat: Update login page and add images
    - Deleted .gitkeep file from assets/images
    - Added HKMU.jpg and MyHKMU.gif to assets/images
    - Modified styles in public/styles.css
    - Updated src/pages/Login.vue with new layout and styles

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="login">
-    <h1>Login Page</h1>
-    <form @submit.prevent="login">
-      <div>
-        <label for="username">Username:</label>
-        <input type="text" id="username" v-model="username" required />
-      </div>
-      <div>
-        <label for="password">Password:</label>
-        <input type="password" id="password" v-model="password" required />
-      </div>
-      <button type="submit">Login</button>
-    </form>
-    <p v-if="message">{{ message }}</p>
+    <img src="../../assets/images/HKMU.jpg" alt="HKMU Logo" class="hkmu-logo" />
+    <div class="login-container">
+      <img
+        src="../../assets/images/MyHKMU.gif"
+        alt="MyHKMU Logo"
+        class="myhkmu-logo"
+      />
+      <h2>Please Log in</h2>
+      <form @submit.prevent="login">
+        <div class="form-group">
+          <label for="username">Email</label>
+          <input type="email" id="username" v-model="username" required />
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model="password" required />
+        </div>
+        <a href="#" class="forgot-password">Forgot password?</a>
+        <button type="submit" class="login-button">Sign In</button>
+      </form>
+      <p v-if="message" class="message">{{ message }}</p>
+    </div>
   </div>
 </template>
 
@@ -31,10 +40,13 @@ export default {
   methods: {
     async login() {
       try {
-        const response = await axios.post("http://localhost:3000/api/login", {
-          username: this.username,
-          password: this.password,
-        });
+        const response = await axios.post(
+          "http://localhost:3000/api/auth/login",
+          {
+            username: this.username,
+            password: this.password,
+          }
+        );
         this.message = response.data.message;
       } catch (error) {
         this.message = error.response.data.message;
@@ -44,6 +56,134 @@ export default {
 };
 </script>
 
-<style>
-/* Add your styles here */
+<style scoped>
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #f5f5f5;
+  position: relative;
+}
+
+.hkmu-logo {
+  width: 280px;
+  height: 68px;
+  position: absolute;
+  top: 20px;
+  left: 20px;
+}
+
+.login-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  padding: 2rem;
+  border-radius: 8px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  text-align: center;
+  width: 1440px;
+  height: 1080px;
+}
+
+.myhkmu-logo {
+  position: absolute;
+  width: 261px;
+  height: 73px;
+  left: calc(50% - 261px / 2 + 0.5px);
+  top: 102px;
+}
+
+h2 {
+  position: absolute;
+  width: 259px;
+  height: 29px;
+  left: calc(50% - 259px / 2 + 0.5px);
+  top: 198px;
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 140%;
+  text-align: center;
+  text-decoration-line: underline;
+  color: #000000;
+}
+
+.form-group {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+  gap: 8px;
+  width: 272px;
+  height: 70px;
+  position: absolute;
+}
+
+.form-group:nth-child(1) {
+  left: calc(50% - 272px / 2);
+  top: 268px;
+}
+
+.form-group:nth-child(2) {
+  left: calc(50% - 272px / 2);
+  top: 350px;
+}
+
+label {
+  display: block;
+  margin-bottom: 0.5rem;
+  font-weight: bold;
+}
+
+input {
+  width: 100%;
+  height: 40px;
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+.forgot-password {
+  display: block;
+  margin-bottom: 1rem;
+  color: #007bff;
+  text-decoration: none;
+  position: absolute;
+  width: 138px;
+  height: 22px;
+  left: calc(50% - 138px / 2);
+  top: 443px;
+}
+
+.forgot-password:hover {
+  text-decoration: underline;
+}
+
+.login-button {
+  width: 290px;
+  height: 40px;
+  padding: 0.75rem;
+  background-color: #28a745;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  font-size: 1rem;
+  cursor: pointer;
+  position: absolute;
+  left: calc(50% - 272px / 2);
+  top: 484px;
+}
+
+.login-button:hover {
+  background-color: #218838;
+}
+
+.message {
+  margin-top: 1rem;
+  color: red;
+}
 </style>
